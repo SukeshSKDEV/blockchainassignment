ace361293e49af67e4e145bdf9310bb48df5581e
SukeshSKDEV <111371880+SukeshSKDEV@users.noreply.github.com>
2022-11-19 22:24:34 +0530

    Delete problem2.js

--- assignment10/problem2.js
+++
@@ -1,1 +0,0 @@
-// Yes.filter is a built-in Higher Order function because it takes callback function as an argument. As we know, Higher order functions are those functions which either accepts or returns another function.

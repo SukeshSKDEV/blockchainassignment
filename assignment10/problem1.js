6b92fd2a15978027a25af5e6a3aadd69bf7efcbe
SukeshSKDEV <111371880+SukeshSKDEV@users.noreply.github.com>
2022-11-19 22:24:25 +0530

    Delete problem1.js

--- assignment10/problem1.js
+++
@@ -1,4 +0,0 @@
-// 1. No they are not same
-// Higher Order Functions : Functions that operate on/with other functions.
-// They can : a. Accept other functions as argument  b. Return a function
-// Callback functions : A function that is passed in another function

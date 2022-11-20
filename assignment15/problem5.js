091719cc54104ef086b748ccd4c602736b3d0a07
SukeshSKDEV <111371880+SukeshSKDEV@users.noreply.github.com>
2022-11-20 14:18:47 +0530

    Update problem5.js

--- assignment15/problem5.js
+++ assignment15/problem5.js
@@ -1,16 +1,3 @@
-// To illustrate: changeEnough([25, 20, 5, 0], 4.25) should yield true,
-// since having 25 quarters, 20 dimes, 5 nickels and 0 pennies gives
-// you 6.25 + 2 + .25 + 0 = 8.50.
-// Examples
-// changeEnough([2, 100, 0, 0], 14.11) ➞ false
-// changeEnough([0, 0, 20, 5], 0.75) ➞ true
-// changeEnough([30, 40, 20, 5], 12.55) ➞ true
-// Notes
-// ● quarter: 25 cents / $0.25
-// ● dime: 10 cents / $0.10
-// ● nickel: 5 cents / $0.05
-// ● penny: 1 cent / $0.01
-
 const changeEnough = (arr, due) => {
   let result = 0;
   for (let i = 0; i < arr.length; i++) {

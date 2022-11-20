529fe1a840a33348521614f78c1ee7f8991951ac
SukeshSKDEV <111371880+SukeshSKDEV@users.noreply.github.com>
2022-11-20 17:47:35 +0530

    Update problem7.js

--- assignment20/problem7.js
+++ assignment20/problem7.js
@@ -1,2 +1,2 @@
 // 7. Find output
-// Answer : 15               Expl=> x + y + z + (await p) + (await q)=1+2+3+4+5=15 print after 15 seconds
+// Answer : 15               Expl=> x + y + z + (await p) + (await q)=1+2+3+4+5=15 print after 7 seconds
